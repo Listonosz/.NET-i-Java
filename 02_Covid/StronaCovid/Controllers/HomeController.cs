078573c1c0859a52ad7852b0ebc1361e937c632d
Daniel <daniel.jablonski1998@gmail.com>
2020-05-12 05:02:11 +0200

    parsing csv to json

--- 02_Covid/StronaCovid/Controllers/HomeController.cs
+++ 02_Covid/StronaCovid/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace StronaCovid.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ 02_Covid/StronaCovid/Data/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using ChoETL;
+using System.Text;
+
+namespace StronaCovid.Data
+{
+    public class Data
+    {
+        public void parsing()
+        {
+            string namecsv = "stats.csv";
+            string namejson = "stats.json";
+            string link = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-10-2020.csv";
+            string filePath = Directory.GetCurrentDirectory();
+            string filecsv = filePath + namecsv;
+            string filejson = filePath + namejson;
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(link, filecsv);
+            }
+        
+            StringBuilder sb = new StringBuilder();
+            using (var p = new ChoCSVReader(filecsv).WithFirstLineHeader())
+            {
+                using (var w = new ChoJSONWriter(filejson))
+                    w.Write(p);
+            }
+        }
+    }
+}
